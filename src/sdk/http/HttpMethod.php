de8b51c10206c3a1a1fe63fad1b6264d9f12911b
linqr <59353073@qq.com>
2023-03-12 14:45:28 +0800

    init

---
+++ src/sdk/http/HttpMethod.php
@@ -0,0 +1,34 @@
+<?php
+namespace Qiyuesuo\sdk\http;
+
+class HttpMethod {
+    /**
+     * DELETE方法。
+     */
+    const DELETE = "delete";
+
+    /**
+     * GET方法。
+     */
+    const GET = "get";
+
+    /**
+     * HEAD方法。
+     */
+    const HEAD = "head";
+
+    /**
+     * POST方法。
+     */
+   const POST = "post";
+
+    /**
+     * PUT方法。
+     */
+    const PUT = "put";
+
+    /**
+     * OPTION方法。
+     */
+    const OPTIONS = "option";
+}
---
+++ src/sdk/request/SdkRequest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Qiyuesuo\sdk\request;
+
+/**
+ * 请求参数基本接口
+ */
+
+abstract class SdkRequest {
+
+    /**
+     * 请求路径
+     * @return mixed
+     */
+    abstract function getUrl();
+
+    /**
+     * 创建请求参数
+     * @return mixed
+     */
+    abstract function getHttpParamers();
+
+
+}
